88bd97a78a271627221233805164ac8e1cb3db29
Hubert Badocha <badocha.hubert@gmail.com>
2022-07-11 19:19:01 +0200

    Added persistence

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 
 module Main where
 
@@ -12,36 +15,14 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Text (Text)
 import qualified Data.Text
+import Data.Text.Lazy (toStrict)
 import GHC.Generics (Generic)
+import Lib
 import Secret (botKey)
 import Telegram.Bot.API
 import Telegram.Bot.Simple
 import Telegram.Bot.Simple.UpdateParser
 import Text.Pretty.Simple (pShowNoColor)
-import Data.Text.Lazy (toStrict)
-
-type Role = Text
-
-data Mention
-  = Username Text
-  | TelegramId UserId Text
-  deriving (Eq, Generic, Show)
-
-instance Ord Mention where
-  (<=) (Username _) (TelegramId _ _) = True
-  (<=) (TelegramId _ _) (Username _) = False
-  (<=) (Username a) (Username b) = a <= b
-  (<=) (TelegramId (UserId a) _) (TelegramId (UserId b) _) = a <= b
-
-newtype Model = Model
-  { roles :: HashMap Role (Set Mention)
-  }
-
-initialModel :: Model
-initialModel =
-  Model
-    { roles = HashMap.empty
-    }
 
 data Action
   = Help
@@ -49,14 +30,42 @@ data Action
   | RemoveRole (Text, [MessageEntity])
   | CreateRole Text
   | ListRoles
-  | Mention (Text,MessageId)
+  | Mention (Text, MessageId)
   deriving (Show)
+
+newtype ReplyMessageM = ReplyMessageM (Maybe ReplyMessage)
+
+newtype TextM = TextM (Maybe Text)
+
+instance GetAction ReplyMessageM a where
+  getNextAction effect = getNextAction do
+    (ReplyMessageM t) <- effect
+    case t of
+      Nothing -> pure ()
+      Just m -> reply m
+
+instance GetAction TextM a where
+  getNextAction effect = getNextAction do
+    (TextM t) <- effect
+    case t of
+      Nothing -> pure ()
+      Just txt -> replyText txt
+
+initialModel :: Model
+initialModel =
+  Model
+    { roles = HashMap.empty,
+      loadedFromDisk = False
+    }
 
 toInt :: UserId -> Int
 toInt (UserId uid) = fromInteger uid
 
 entities :: UpdateParser [MessageEntity]
 entities = UpdateParser (updateMessage >=> messageEntities)
+
+messageChatId :: UpdateParser ChatId
+messageChatId = UpdateParser (updateMessage >=> (Just . chatId . messageChat))
 
 command' :: Text -> UpdateParser (Text, [MessageEntity])
 command' name = do
@@ -69,10 +78,7 @@ command' name = do
     _ -> fail "not that command"
 
 messageId :: UpdateParser MessageId
-messageId = UpdateParser (updateMessage >=> mmId)
-  where
-  mmId :: Message -> Maybe MessageId
-  mmId = Just . messageMessageId
+messageId = UpdateParser (updateMessage >=> Just . messageMessageId)
 
 mention :: UpdateParser (Text, MessageId)
 mention = do
@@ -82,83 +88,109 @@ mention = do
     then pure (t, mid)
     else fail "not mention"
 
-rolesBot :: BotApp Model Action
+addChatId :: UpdateParser a -> UpdateParser (ChatId, a)
+addChatId m = m >>= (\a -> fmap (,a) messageChatId)
+
+rolesBot :: BotApp (IO Model) (ChatId, Action)
 rolesBot =
   BotApp
-    { botInitialModel = initialModel,
+    { botInitialModel = return initialModel,
       botAction = flip updateToAction,
       botHandler = handleAction,
       botJobs = []
     }
   where
-    updateToAction :: Model -> Update -> Maybe Action
+    updateToAction :: IO Model -> Update -> Maybe (ChatId, Action)
     updateToAction _ =
       parseUpdate $
-        Mention <$> mention
-          <|> Help <$ command "help"
-          <|> AddRole <$> command' "role_add"
-          <|> RemoveRole <$> command' "role_remove"
-          <|> CreateRole <$> command "role_create"
-          <|> ListRoles <$ command "role_list"
+        addChatId $
+          Mention <$> mention
+            <|> Help <$ command "help"
+            <|> AddRole <$> command' "role_add"
+            <|> RemoveRole <$> command' "role_remove"
+            <|> CreateRole <$> command "role_create"
+            <|> ListRoles <$ command "role_list"
 
-    handleAction :: Action -> Model -> Eff Action Model
-    handleAction action model = case action of
-      Help ->
-        model <# do
-          replyText helpMessage
-      AddRole (msg, ents) ->
-        case validateAddRole msg model of
-          Nothing -> addRole msg ents model <# pure ()
-          Just err -> model <# replyText err
-      RemoveRole (msg, ents) ->
-        case validateRemoveRole msg model of
-          Nothing -> removeRole msg ents model <# pure ()
-          Just err -> model <# replyText err
-      CreateRole msg ->
-        case validateCreateRole msg model of
-          Nothing -> createRole msg model <# pure ()
-          Just err -> model <# replyText err
-      ListRoles ->
-        model <# do
-          replyText $ toStrict $ pShowNoColor $ roles model
-      Mention (t, mid) ->
-        model <# do
-          handleMention t mid model
-
-    helpMessage =
-      Data.Text.unlines
-        [
-          "This bot brings roles features to your Telegram chat!",
-          "",
-          "`/role_create <role_name>*` creates roles",
-          "`/role_add <role_name> <mention>*` adds role to the mentioned users",
-          "`/role_remove <role_name> <mention>*` removes role from the mentioned users",
-          "`/role_list` list roles and assigned people"
-        ]
-
-handleMention :: Text -> MessageId -> Model -> BotM ()
-handleMention t mid Model {roles} =
-  case HashMap.lookup (getMention t) roles of
-    Just users -> reply $ createMsg users
-    Nothing -> pure ()
-  where
-    createMsg users =
-      let (msgText, ents) = createMsgTextAndEntities users
-       in ReplyMessage msgText Nothing (Just ents) Nothing Nothing Nothing (Just mid) Nothing Nothing
-    createMsgTextAndEntities users = Set.foldl createMsg' (Data.Text.empty, []) users
-    createMsg' :: (Text, [MessageEntity]) -> Mention -> (Text, [MessageEntity])
-    createMsg' (txt, e) (Username username) = (addToBack txt username, e)
-    createMsg' (txt, e) u@(TelegramId _ name) =
-      let offset = Data.Text.length txt + 1
-          nT = addToBack txt name
-          ent = createEntity offset u
-       in (nT, ent : e)
-    createEntity offset u@(TelegramId _ name) =
-      let len = Data.Text.length name
-       in MessageEntity MessageEntityTextMention offset len Nothing (Just $ createUser u) Nothing
-    createUser (TelegramId telegramId name) = User telegramId False name Nothing Nothing Nothing Nothing Nothing Nothing
-    addToBack start back = Data.Text.intercalate (Data.Text.pack " ") [start, back]
-    getMention = Data.Text.drop 1 . head . Data.Text.words
+    handleAction :: (ChatId, Action) -> IO Model -> Eff (ChatId, Action) (IO Model)
+    handleAction (chatId, action) model =
+      let m = model >>= tryLoadFromDisk chatId
+       in do
+            case action of
+              Help ->
+                m <# replyText helpMessage
+              AddRole (msg, ents) -> handleAddRole msg ents m
+              RemoveRole (msg, ents) -> handleRemoveRole msg ents m
+              CreateRole msg -> handleCreateRole msg m
+              ListRoles -> handleListRoles m
+              Mention (t, mid) ->
+                m <# do
+                  handleMention t mid m
+      where
+        handleAddRole :: Text -> [MessageEntity] -> IO Model -> Eff (ChatId, Action) (IO Model)
+        handleAddRole msg ents m = saveModel newModel <# botM
+          where
+            validation = validateAddRole msg <$> m
+            newModel = validation >>= newModelF
+            newModelF Nothing = addRole msg ents <$> m
+            newModelF (Just _) = m
+            botM = liftIO $ TextM <$> validation
+        handleRemoveRole :: Text -> [MessageEntity] -> IO Model -> Eff (ChatId, Action) (IO Model)
+        handleRemoveRole msg ents m = saveModel newModel <# botM
+          where
+            validation = validateRemoveRole msg <$> m
+            newModel = validation >>= newModelF
+            newModelF Nothing = removeRole msg ents <$> m
+            newModelF (Just _) = m
+            botM = liftIO $ TextM <$> validation
+        handleCreateRole :: Text -> IO Model -> Eff (ChatId, Action) (IO Model)
+        handleCreateRole msg m = saveModel newModel <# botM
+          where
+            validation = validateCreateRole msg <$> m
+            newModel = validation >>= newModelF
+            newModelF Nothing = createRole msg <$> m
+            newModelF (Just _) = m
+            botM = liftIO $ TextM <$> validation
+        handleListRoles :: IO Model -> Eff (ChatId, Action) (IO Model)
+        handleListRoles m = m <# liftIO listRoleMsg
+          where
+            listRoleMsg = Data.Text.Lazy.toStrict . pShowNoColor . roles <$> m
+        saveModel :: IO Model -> IO Model
+        saveModel m = m >>= saveToDisk chatId >> m
+        helpMessage =
+          Data.Text.unlines
+            [ "This bot brings roles features to your Telegram chat!",
+              "",
+              "`/role_create <role_name>*` creates roles",
+              "`/role_add <role_name> <mention>*` adds role to the mentioned users",
+              "`/role_remove <role_name> <mention>*` removes role from the mentioned users",
+              "`/role_list` list roles and assigned people"
+            ]
+        handleMention :: Text -> MessageId -> IO Model -> BotM ReplyMessageM
+        handleMention t mid mM =
+          liftIO $
+            ( \m -> ReplyMessageM $ case HashMap.lookup (getMention t) (roles m) of
+                Just users -> Just $ createMsg users
+                Nothing -> Nothing
+            )
+              <$> mM
+          where
+            createMsg users =
+              let (msgText, ents) = createMsgTextAndEntities users
+               in ReplyMessage msgText Nothing (Just ents) Nothing Nothing Nothing (Just mid) Nothing Nothing
+            createMsgTextAndEntities users = Set.foldl createMsg' (Data.Text.empty, []) users
+            createMsg' :: (Text, [MessageEntity]) -> Mention -> (Text, [MessageEntity])
+            createMsg' (txt, e) (Username username) = (addToBack txt username, e)
+            createMsg' (txt, e) u@(TelegramId _ name) =
+              let offset = Data.Text.length txt + 1
+                  nT = addToBack txt name
+                  ent = createEntity offset u
+               in (nT, ent : e)
+            createEntity offset u@(TelegramId _ name) =
+              let len = Data.Text.length name
+               in MessageEntity MessageEntityTextMention offset len Nothing (Just $ createUser u) Nothing
+            createUser (TelegramId telegramId name) = User telegramId False name Nothing Nothing Nothing Nothing Nothing Nothing
+            addToBack start back = Data.Text.intercalate (Data.Text.pack " ") [start, back]
+            getMention = Data.Text.drop 1 . head . Data.Text.words
 
 validateAddRole :: Text -> Model -> Maybe Text
 validateAddRole t Model {roles} =
@@ -172,13 +204,12 @@ validateCreateRole t Model {roles} =
   let parts = Data.Text.words t
       checkIfExists acc name =
         if HashMap.member name roles
-        then Data.Text.intercalate Data.Text.empty [Data.Text.pack "Role: `", name, Data.Text.pack "` exists!"] : acc
-        else acc
+          then Data.Text.intercalate Data.Text.empty [Data.Text.pack "Role: `", name, Data.Text.pack "` exists!"] : acc
+          else acc
       errors = foldl checkIfExists [] parts
-  in
-    case errors of
-      [] -> Nothing
-      _ -> Just $ Data.Text.unlines errors
+   in case errors of
+        [] -> Nothing
+        _ -> Just $ Data.Text.unlines errors
 
 validateRemoveRole :: Text -> Model -> Maybe Text
 validateRemoveRole = validateAddRole
@@ -193,15 +224,15 @@ addRole msg ents =
    in addToRole role users
 
 removeRole :: Text -> [MessageEntity] -> Model -> Model
-removeRole msg ents model =
+removeRole msg ents =
   let users = parseMentions msg ents
       role = getRole msg
-   in removeFromRole role users model
+   in removeFromRole role users
 
 createRole :: Text -> Model -> Model
 createRole rolesMsg model =
   let parts = Data.Text.words rolesMsg
-      addPart Model {roles} name = Model {roles = HashMap.insertWith (\_ x -> x) name Set.empty roles}
+      addPart m@Model {roles} name = m {roles = HashMap.insertWith (\_ x -> x) name Set.empty roles}
    in foldl addPart model parts
 
 addToRole :: Role -> [Mention] -> Model -> Model
@@ -210,14 +241,14 @@ addToRole role users model@Model {roles} =
     Nothing -> model
     Just prev ->
       let updated = foldl (flip Set.insert) prev users
-       in Model {roles = HashMap.insert role updated roles}
+       in model {roles = HashMap.insert role updated roles}
 
 removeFromRole :: Role -> [Mention] -> Model -> Model
 removeFromRole _ [] model = model
-removeFromRole role (u : t) Model {roles} =
+removeFromRole role (u : t) m@Model {roles} =
   removeFromRole role t model
   where
-    model = Model {roles = HashMap.alter alterFn role roles}
+    model = m {roles = HashMap.alter alterFn role roles}
     alterFn Nothing = Nothing
     alterFn (Just x) = Just $ Set.delete u x
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,113 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import Data.Bifunctor (bimap)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as ByteString
+import Data.HashMap.Internal.Strict (HashMap)
+import qualified Data.HashMap.Internal.Strict as HashMap
+import Data.Serialize (Serialize, decode, encode)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Text (Text, pack, unpack)
+import GHC.Generics (Generic)
+import System.AtomicWrite.Writer.ByteString
+import System.Directory
+import Telegram.Bot.API
+import System.FilePath (joinPath)
+
+type Role = Text
+
+type Roles = HashMap Role (Set Mention)
+
+data Model = Model
+  { roles :: Roles,
+    loadedFromDisk :: Bool
+  }
+  deriving (Generic)
+
+tryLoadFromDisk :: ChatId -> Model -> IO Model
+tryLoadFromDisk chatId model@Model {loadedFromDisk} =
+  if loadedFromDisk
+    then return model
+    else loadFromDisk chatId
+
+data Mention
+  = Username Text
+  | TelegramId UserId Text
+  deriving (Eq, Generic, Show)
+
+instance Ord Mention where
+  (<=) (Username _) (TelegramId _ _) = True
+  (<=) (TelegramId _ _) (Username _) = False
+  (<=) (Username a) (Username b) = a <= b
+  (<=) (TelegramId (UserId a) _) (TelegramId (UserId b) _) = a <= b
+
+saveToDisk :: ChatId -> Model -> IO ()
+saveToDisk chat model = do
+  let serialized = serialize model
+  let name = getFilePath chat
+  atomicWriteFile name serialized
+
+newtype MentionSerialized
+  = MentionSerialized (String, Maybe Integer)
+  deriving (Serialize)
+
+newtype SerializedRoles = SerializedRoles [(String, [MentionSerialized])] deriving (Serialize)
+
+serialize :: Model -> ByteString
+serialize Model {roles} = encode $ toSerializedRoles roles
+  where
+    toSerializedRoles :: Roles -> SerializedRoles
+    toSerializedRoles rs = SerializedRoles $ map (Data.Bifunctor.bimap unpack serializeMentions) $ HashMap.toList rs
+    serializeMentions :: Set Mention -> [MentionSerialized]
+    serializeMentions mentions = map serializeMention $ Set.toList mentions
+    serializeMention :: Mention -> MentionSerialized
+    serializeMention (Username name) = MentionSerialized (Data.Text.unpack name, Nothing)
+    serializeMention (TelegramId (UserId uid) name) = MentionSerialized (Data.Text.unpack name, Just uid)
+
+deserialize :: ByteString -> Either String Model
+deserialize serialized = fromSerializedRoles <$> decode serialized
+  where
+    fromSerializedRoles :: SerializedRoles -> Model
+    fromSerializedRoles (SerializedRoles sr) = Model {roles = roles, loadedFromDisk = True}
+      where
+        roles = HashMap.fromList $ map (Data.Bifunctor.bimap pack deserializeMentions) sr
+        deserializeMentions ms = Set.fromList $ map deserializeMention ms
+        deserializeMention :: MentionSerialized -> Mention
+        deserializeMention (MentionSerialized (name, Nothing)) = Username $ Data.Text.pack name
+        deserializeMention (MentionSerialized (name, Just tid)) = TelegramId (UserId tid) (Data.Text.pack name)
+
+defaultModel :: Model
+defaultModel =
+  Model
+    { roles = HashMap.empty,
+      loadedFromDisk = True
+    }
+
+loadFromDisk :: ChatId -> IO Model
+loadFromDisk chat = do
+  let name = getFilePath chat
+  exists <- doesFileExist name
+  if exists
+    then do
+      ByteString.readFile name
+        >>= ( \case
+                Left err -> putStrLn err >> return defaultModel
+                Right m -> return m
+            )
+          . deserialize
+    else return defaultModel
+
+getFilePath :: ChatId -> String
+getFilePath cid = joinPath [serializedFolderName, getFileName cid]
+
+serializedFolderName :: String
+serializedFolderName = "serialized"
+
+getFileName :: ChatId -> String
+getFileName (ChatId cid) = show cid
